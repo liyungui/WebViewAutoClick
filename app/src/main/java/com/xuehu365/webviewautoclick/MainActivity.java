1138dc39b72d434f5615e1e2a447680b166ead23
David <david.l@xuehu365.com>
2017-03-10 11:12:43 +0800

    commit

---
+++ app/src/main/java/com/xuehu365/webviewautoclick/MainActivity.java
@@ -0,0 +1,52 @@
+package com.xuehu365.webviewautoclick;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String[] DOM_IDS = {"link"};// 需要操作的dom元素id
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webview);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.d("lyg", "shouldOverrideUrlLoading " + url);
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Log.d("lyg", "onPageFinished " + url);
+                view.loadUrl(getDomOperationStatements(DOM_IDS));
+            }
+        });
+        webView.loadUrl("file:///android_asset/schame-test.html");
+    }
+
+    public static String getDomOperationStatements(String[] domIds) {
+        StringBuilder builder = new StringBuilder();
+        // add javascript prefix
+        builder.append("javascript:(function() { ");
+        for (String domId : domIds) {
+            builder.append("var item = document.getElementById('").append(domId).append("');");
+            builder.append("item.click();");
+        }
+        // add javascript suffix
+        builder.append("})()");
+        return builder.toString();
+    }
+}
---
+++ reademe.md
@@ -0,0 +1,6 @@
+	03-10 11:06:41.989 17549-17549/com.xuehu365.webviewautoclick D/lyg: onPageFinished file:///android_asset/schame-test.html
+	03-10 11:06:41.993 17549-17549/com.xuehu365.webviewautoclick D/lyg: shouldOverrideUrlLoading http://192.168.1.21/wp
+	03-10 11:06:42.000 17549-17549/com.xuehu365.webviewautoclick D/lyg: shouldOverrideUrlLoading http://192.168.1.21/wp/
+	03-10 11:06:43.246 17549-17549/com.xuehu365.webviewautoclick D/lyg: onPageFinished http://192.168.1.21/wp/
+
+后台统计页面被访问了 1 次
